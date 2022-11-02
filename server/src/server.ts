bfe0fecfb27ad760c3b03ee784cc99b6a9272650
Pablo Alan <pabloxt14@gmail.com>
2022-11-02 09:39:46 -0300

    Revert "feat: fixed bug in prisma-erd-generator, finished back-end part"
    This reverts commit 6e48ee3b5173eb2a901c6639848d5ac69381f326.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,4 @@
 import Fastify from 'fastify'
-import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const PORT = 3333
@@ -12,18 +11,13 @@ async function bootstrap() {
     logger: true, // printar no terminal tudo que acontecer em nosso servidor 
   })
 
-  // PERMITINDO QUE QLQ FRONT-END ACESSE NOSSO BACK-END
-  await fastify.register(cors, {
-    origin: true,
-  })
-
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: PORT, host: '0.0.0.0' })
+  await fastify.listen({ port: PORT })
 }
 
 bootstrap()
