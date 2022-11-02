6e48ee3b5173eb2a901c6639848d5ac69381f326
Pablo Alan <pabloxt14@gmail.com>
2022-11-01 22:52:39 -0300

    feat: fixed bug in prisma-erd-generator, finished back-end part

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const PORT = 3333
@@ -11,13 +12,18 @@ async function bootstrap() {
     logger: true, // printar no terminal tudo que acontecer em nosso servidor 
   })
 
+  // PERMITINDO QUE QLQ FRONT-END ACESSE NOSSO BACK-END
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({ port: PORT })
+  await fastify.listen({ port: PORT, host: '0.0.0.0' })
 }
 
 bootstrap()
